5cdffc6f62e6c2ed7f7f8a10d34b8779a2e27807
Danielle Cardona <danielle.cardona23@gmail.com>
2024-10-29 16:04:54 -0600

    feat: add create merchant route and tests

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -5,6 +5,18 @@ class Api::V1::MerchantsController < ApplicationController
     render json: MerchantSerializer.new(merchants)
   end
 
+  def create
+      begin
+        merchant = Merchant.create!(merchant_params)
+        render json: MerchantSerializer.new(merchant), status: :created
+      rescue ActiveRecord::RecordInvalid => exception
+        render json: {
+          'message': "your query could not be completed",
+          'errors': exception.record.errors.full_messages
+      }, status: :bad_request
+    end
+  end
+
   def show
     merchant = Merchant.find(params[:id])
     render json: MerchantSerializer.new(merchant)
@@ -13,4 +25,9 @@ class Api::V1::MerchantsController < ApplicationController
   def destroy
     render json: Merchant.delete(params[:id])
   end
+
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
+
 end
