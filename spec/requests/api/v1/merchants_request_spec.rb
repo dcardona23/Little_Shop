ea4d295dcb2abf0cb4e66b7cbe3ba280fc9d9941
Michael OBrien <obrien.michael.t@gmail.com>
2024-10-29 17:08:29 -0400

    Added destroy route for merchants

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,7 +10,6 @@ describe "Merchants" do
     get "/api/v1/merchants/#{@merchant1.id}"
     merchant = JSON.parse(response.body)
     
-    binding.pry
     expect(merchant["data"]).to have_key("id")
     expect(merchant["data"]["attributes"]).to have_key("name")
     expect(merchant["data"]["attributes"]["name"]).to eq(@merchant1.name)
