99bfe3c99450e8dcf4194476088b32177967ab6d
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-10-29 14:04:56 -0500

    reverted changesitems

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,10 +2,4 @@ class ItemSerializer
   include JSONAPI::Serializer
   set_id :id
   attributes :name, :description, :unit_price, :merchant_id
-
-  def self.record_hash(record, fieldset, includes_list, params)
-    hash = super
-    hash[:id] = record.id.to_i
-    hash
-  end
 end
