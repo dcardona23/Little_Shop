83772b99153fce0f102bb69f503c4314ea3d1cb2
Michael OBrien <obrien.michael.t@gmail.com>
2024-10-29 16:17:50 -0400

    Added the index route for merchants

--- spec/requests/api/v1/merchants_spec.rb
+++ spec/requests/api/v1/merchants_spec.rb
@@ -1,2 +1,20 @@
 require 'rails_helper'
 
+describe "Merchants" do
+  before :each do
+    @merchant1 = Merchant.create(name: "Little Shop of Horrors")
+    @merchant2 = Merchant.create(name: "Large Shop of Wonders")
+  end
+
+  it 'can get a merchant' do
+    binding.pry
+    get "/api/v1/merchants/#{@merchant1.id}"
+    merchant = JSON.parse(response.body)
+
+    expect(merchant[data]).to have_key("id")
+    expect(merchant["data"]["id"]).to be_an(Integer)
+
+    expect(merchant["data"]["attributes"]).to have_key("name")
+    expect(merchant["data"]["attributes"]["name"]).to be_a(String)
+  end
+end
