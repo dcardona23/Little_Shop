2cb2512f7305badc50b058b2f1852d81b0ff2062
Danielle Cardona <danielle.cardona23@gmail.com>
2024-10-29 12:02:18 -0600

    refactor: modify controller to enable serializer gem to work

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::MerchantsController < ApplicationController
 
   def show
     merchant = Merchant.find(params[:id])
-    render json: MerchantSerializer.format_merchant(merchant)
+    render json: MerchantSerializer.new(merchant)
   end
 end
