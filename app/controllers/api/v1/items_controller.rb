39340d84b97588daac296c798fb809a8345758b6
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-10-29 15:25:57 -0500

    created create method and params helper method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,11 +7,18 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(items, options)
   end
 
-
+  def create
+    item = Item.create(item_param)
+    render json: ItemSerializer.new(item)
+  end
 
 
 
   private
+
+  def item_param
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 
   def sort_items(scope)
     case params[:sorted]
