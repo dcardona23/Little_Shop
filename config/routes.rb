c46b2dd810b185b459e0f87970f6e91181978035
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-10-29 15:26:21 -0500

    updated routes with post#create

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   get "/api/v1/merchants/:id", to: "api/v1/merchants#show"
 
   get "/api/v1/items", to: "api/v1/items#index"
+  post "/api/v1/items", to: "api/v1/items#create"
 end
