6c89f913a2c21411460a6ddd20a570b4aad3f5b9
Bryan Willett <btwillett2003@gmail.com>
2024-10-29 16:25:35 -0500

    feat:Items show with rspec test suite.

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   get "/api/v1/merchants/:id", to: "api/v1/merchants#show"
 
   get "/api/v1/items", to: "api/v1/items#index"
+  get "/api/v1/items/:id", to: "api/v1/items#show"
 end
