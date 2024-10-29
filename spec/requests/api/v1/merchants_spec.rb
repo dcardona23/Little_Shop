ef2bc1f142d3ac172189338fb908f03d351c4e69
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-10-29 16:03:58 -0500

    removed binding.pry

--- spec/requests/api/v1/merchants_spec.rb
+++ spec/requests/api/v1/merchants_spec.rb
@@ -7,12 +7,11 @@ describe "Merchants" do
   end
 
   it 'can get a merchant' do
-    binding.pry
     get "/api/v1/merchants/#{@merchant1.id}"
     merchant = JSON.parse(response.body)
 
-    expect(merchant[data]).to have_key("id")
-    expect(merchant["data"]["id"]).to be_an(Integer)
+    expect(merchant["data"]).to have_key("id")
+    expect(merchant["data"]["id"]).to be_an(String)
 
     expect(merchant["data"]["attributes"]).to have_key("name")
     expect(merchant["data"]["attributes"]["name"]).to be_a(String)
