71fed3b6da324b22bd4c7eeea6e8a36cbdb6f26c
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-10-29 18:02:23 -0500

    added status code to post#create

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -25,7 +25,7 @@ class Api::V1::ItemsController < ApplicationController
   
   def create
     item = Item.create(item_param)
-    render json: ItemSerializer.new(item)
+    render json: ItemSerializer.new(item), status: :created
   end
 
   def destroy
