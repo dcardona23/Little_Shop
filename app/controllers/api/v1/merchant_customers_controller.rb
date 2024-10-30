b459c85d1a81710eb347f7b1025b347b4b4e2c41
Michael OBrien <obrien.michael.t@gmail.com>
2024-10-30 19:41:15 -0400

    Controller for get customers for a particular merchant.

--- app/controllers/api/v1/merchant_customers_controller.rb
+++ app/controllers/api/v1/merchant_customers_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::MerchantCustomersController < ApplicationController
   def index
     merchant = Merchant.find(params[:id]).id
     customers = Customer.joins(:invoices).where("merchant_id = ?", merchant)
-    render json: CustomerSerializer.new(customers)
+    render json: CustomerSerializer.new(customers) 
   end
 end
