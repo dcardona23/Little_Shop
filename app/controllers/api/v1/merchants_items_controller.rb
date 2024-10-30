e0db7a1f64b9082919f0ba19aecd59f0f95a0277
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-10-30 16:19:14 -0500

    feat added ability to find all items by a merchants id

--- app/controllers/api/v1/merchants_items_controller.rb
+++ app/controllers/api/v1/merchants_items_controller.rb
@@ -1,8 +1,20 @@
 class Api::V1::MerchantsItemsController < ApplicationController
 
   def index
-    merchant = Merchant.find(params[:id])
-    items = Item.where(merchant_id: merchant.id)
-    render json: ItemSerializer.new(items)
+    begin
+      merchant = Merchant.find(params[:id])
+      items = Item.where(merchant_id: merchant.id)
+      options = {meta: {count: (items.count)}}
+      render json: ItemSerializer.new(items, options)
+    rescue ActiveRecord::RecordNotFound => exception
+      render json: {
+        errors: [
+          {
+            status: "404",
+            title: exception.message
+          }
+        ]
+      }, status: :not_found
+    end
   end
 end
