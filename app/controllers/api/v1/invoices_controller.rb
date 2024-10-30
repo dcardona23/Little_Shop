c5976ac7b470eef6a7325252e1d0eb05d1c27144
Danielle Cardona <danielle.cardona23@gmail.com>
2024-10-29 21:13:12 -0600

    refactor: refactor invoice index method for accurate error handling

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -1,13 +1,29 @@
 class Api::V1::InvoicesController < ApplicationController
 
 	def index
-		merchant = Merchant.find_by(id: params[:id])
-		merchant_id = merchant.id
+		begin
+			merchant = Merchant.find(params[:id])
+			invoices = Invoice.where(merchant_id: merchant.id).find_by_status(params[:status])
 
-		invoices = Invoice.where(merchant_id: merchant_id).find_by_status(params[:status])
+			if invoices.empty? || !merchant.id
+				render json: {
+					'message': "your query could not be completed",
+					'errors': [
+						"no invoices found with the specified status"
+					]
+				}, status: :not_found
+				return 
+			end
 
-		render json: InvoiceSerializer.new(invoices)
+			render json: InvoiceSerializer.new(invoices)
+
+		rescue ActiveRecord::RecordNotFound
+			render json: {
+					'message': "your query could not be completed",
+					'errors': [
+						"merchant not found"
+					]
+				}, status: :not_found
+		end
 	end
 end
-
-#when get request is made for a merchant id, it returns all invoices for that merchant
