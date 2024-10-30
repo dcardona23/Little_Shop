2b2e25a36907885f1d7d4c5a54982dc918af8078
Michael OBrien <obrien.michael.t@gmail.com>
2024-10-30 18:24:09 -0400

    Fixed typo and added CustomersSerializer

--- app/controllers/api/v1/merchant_customers_controller.rb
+++ app/controllers/api/v1/merchant_customers_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::MerchantCustomersController < ApplicationController
   
   def index
     merchant = Merchant.find(params[:id]).id
-    customers = Customer.joins(:invoices).where("merchan_ig = ?", merchant)
+    customers = Customer.joins(:invoices).where("merchant_id = ?", merchant)
+    render json: CustomerSerializer.new(customers)
   end
 end
