ac87b67817372fc3f47b0238d888f37783eaaeb8
Danielle Cardona <danielle.cardona23@gmail.com>
2024-10-31 12:21:46 -0600

    feat: add model testing for sorting merchants

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Merchant do
 
+  before(:each) do
+    merchant1 = Merchant.create(name: "Little Shop of Horrors")
+    merchant2 = Merchant.create(name: "Large Shop of Wonders")
+    merchant3 = Merchant.create(name: "Wizard's Chest")
+  end
+
   describe 'relationships' do
     it {should have_many :invoices}
     it {should have_many :items}
@@ -9,5 +15,18 @@ RSpec.describe Merchant do
 
   describe 'validations' do
     it {should validate_presence_of(:name)}
+  end
+
+  describe 'class methods' do
+    it 'sorts merchants by time created' do
+      sorted_by_age = Merchant.sort_by_age
+      
+      expect(sorted_by_age[0].name).to eq("Little Shop of Horrors")
+      expect(sorted_by_age[1].name).to eq("Large Shop of Wonders")
+      expect(sorted_by_age[2].name).to eq("Wizard's Chest")
+    end
+
+    
+
   end
 end
