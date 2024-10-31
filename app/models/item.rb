a5445131cfbff204334c75c843da876fce83d244
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-10-31 17:02:17 -0500

    Added validation testing

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoiceItems
+
+  validates_presence_of :name, :presence => true
+  validates_presence_of :description, :presence => true
+  validates_presence_of :unit_price, :presence => true
+
+
 end
