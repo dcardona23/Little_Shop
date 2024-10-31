0de344436424f18918a3abfc86dccd062ad6e285
Michael OBrien <obrien.michael.t@gmail.com>
2024-10-31 17:02:04 -0400

    Adds error handling to controller

--- app/controllers/api/v1/merchant_customers_controller.rb
+++ app/controllers/api/v1/merchant_customers_controller.rb
@@ -3,6 +3,15 @@ class Api::V1::MerchantCustomersController < ApplicationController
   def index
     merchant = Merchant.find(params[:id]).id
     customers = Customer.joins(:invoices).where("merchant_id = ?", merchant)
-    render json: CustomerSerializer.new(customers) 
+    render json: CustomerSerializer.new(customers)
+  rescue ActiveRecord::RecordNotFound => error
+    render json: {
+        errors: [
+          {
+            status: "404",
+            message: error.message
+          }
+        ]
+      }, status: 404 #alternative status: :not_found
   end
 end
