5db48c59d8d4364bc898097f6e66f5600d303061
Michael OBrien <obrien.michael.t@gmail.com>
2024-10-31 18:13:52 -0400

    Cleaned up comments

--- app/controllers/api/v1/merchant_customers_controller.rb
+++ app/controllers/api/v1/merchant_customers_controller.rb
@@ -5,15 +5,6 @@ class Api::V1::MerchantCustomersController < ApplicationController
     merchant = Merchant.find(params[:id]).id
     customers = Customer.joins(:invoices).where("merchant_id = ?", merchant)
     render json: CustomerSerializer.new(customers)
-  # rescue ActiveRecord::RecordNotFound => error
-  #   render json: {
-  #       errors: [
-  #         {
-  #           status: "404",
-  #           message: error.message
-  #         }
-  #       ]
-  #     }, status: 404 #alternative status: :not_found
   end
 
   private
