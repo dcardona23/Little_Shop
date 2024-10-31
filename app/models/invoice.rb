83cd9832f938cfd6ddaab82492b467927c34e25c
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-10-31 17:01:54 -0500

    added validation

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,6 +4,8 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoiceItems
 
+  validates_presence_of :status, :presence => true
+
   def self.find_by_status(input)
     where(status: input)
   end
