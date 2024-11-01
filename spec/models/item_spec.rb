068f148f5f929bda0caafaf456aa5b7284583cd8
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-11-01 15:59:25 -0500

    tests: modified tests to match new format

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -63,7 +63,7 @@ RSpec.describe Item do
       items_sorted = Item.sort_items(Item.all, params)
 
       expect(items_sorted[0][:unit_price]).to eq(0.50)
-      # expect(items_sorted[1][:unit_price]).to eq(0.75)
+      expect(items_sorted[1][:unit_price]).to eq(0.75)
       expect(items_sorted[2][:unit_price]).to eq(1.50)
       expect(items_sorted[3][:unit_price]).to eq(3.50)
       params = {sorted: nil}
@@ -109,8 +109,8 @@ RSpec.describe Item do
       unit_price: Faker::Commerce.price(range: 1.0..55.00),
       merchant_id: @merchant1[:id]
     })
-      
-      expect(Item.find_by_name("copper")).to include(item5, item6, item7)
+      params = { name: "copper"}
+      expect(Item.find_by_name(Item.all, params)).to include(item5, item6, item7)
     end
 
     it 'find all items by case insensitive name fragment' do 
@@ -149,10 +149,16 @@ RSpec.describe Item do
         merchant_id: @merchant1[:id]
       })
         
-      expect(Item.find_by_name("copPER")).to include(item5, item6, item7)
-      expect(Item.find_by_name("COPper")).to include(item5, item6, item7)
-      expect(Item.find_by_name("cOPPer")).to include(item5, item6, item7)
-      expect(Item.find_by_name("cOpPeR")).to include(item5, item6, item7)    end
+      param1 = {name: "copPER"}
+      param2 = {name: "COPper"}
+      param3 = {name: "cOPPer"}
+      param4 = {name: "cOpPeR"}
+
+      expect(Item.find_by_name(Item.all,param1)).to include(item5, item6, item7)
+      expect(Item.find_by_name(Item.all,param2)).to include(item5, item6, item7)
+      expect(Item.find_by_name(Item.all,param3)).to include(item5, item6, item7)
+      expect(Item.find_by_name(Item.all, param4)).to include(item5, item6, item7)
+    end
 
     it 'filters by min price' do
       params = {min_price: 0.75}
