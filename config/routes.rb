2015b209309cf241920722613e25de298a72582e
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-11-01 15:58:23 -0500

    changed route to index

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get "/api/v1/merchants/:id/items", to: "api/v1/merchants_items#index"
 
   get "/api/v1/items", to: "api/v1/items#index"
-  get "api/v1/items/find_all", to: "api/v1/items#fetch_by_name"
+  get "/api/v1/items/find_all", to:  "api/v1/items#index"
   get "/api/v1/items/:id", to: "api/v1/items#show"
   patch "/api/v1/items/:id", to: "api/v1/items#update"
   put "/api/v1/items/:id", to: "api/v1/items#update"
@@ -26,4 +26,5 @@ Rails.application.routes.draw do
   delete "/api/v1/items/:id", to: "api/v1/items#destroy"
   get "/api/v1/items/:id/merchant", to: "api/v1/items_merchants#index"
   get "/api/v1/items/:id/merchant", to: "api/v1/items_merchants#index"
+
 end
