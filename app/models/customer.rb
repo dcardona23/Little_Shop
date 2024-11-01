0ff8ffcad52fee919157823927af5c3ab262a594
Michael OBrien <obrien.michael.t@gmail.com>
2024-11-01 19:45:07 -0400

    Added db logic to find all customers for a merchant

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,7 @@ class Customer < ApplicationRecord
   validates_presence_of :first_name, :presence => true
   validates_presence_of :last_name, :presence => true
 
+  def self.customersForMerchant(merchant)
+    Customer.joins(:invoices).where("merchant_id = ?", merchant).uniq
+  end
 end
