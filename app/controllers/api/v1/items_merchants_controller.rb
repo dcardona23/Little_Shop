b18fa5e98e76d1792d10a46a320c85d4e6c40f3e
Bryan Willett <btwillett2003@gmail.com>
2024-10-31 20:35:51 -0500

    Refactored items_merchants_controller for ErrorSerializer and added one RSpec test. Deleted duplicate route.

--- app/controllers/api/v1/items_merchants_controller.rb
+++ app/controllers/api/v1/items_merchants_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::ItemsMerchantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
     item = Item.find(params[:id])
     merchant = Merchant.find(item.merchant_id)
     render json: MerchantIndexSerializer.new(merchant)
+  end
+
+  private
+
+  def record_not_found(exception)
+    render json: ErrorSerializer.format_error(exception, 404), status: :not_found
   end
 end
