66479d5b55a0dcb4ee39d6ef7633c1fae027e36d
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-11-01 18:34:40 -0500

    added validation testing for parameters non restful search

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::ItemsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
-
+  rescue_from ArgumentError, with: :invalid_parameters
   def index
     items = Item.all
+    validate_params
     items = Item.sort_items(items, params)
     items = Item.filter_items(items, params)
     options = { meta: { count: items.count } }
@@ -44,5 +45,17 @@ class Api::V1::ItemsController < ApplicationController
     render json: ErrorSerializer.format_error(exception, 404), status: :not_found
   end
 
-  
+  def invalid_parameters(exception)
+    render json: ErrorSerializer.format_error(exception, 400), status: :bad_request
+  end
+
+  def validate_params
+    if params[:name].present? && (params[:min_price].present? || params[:max_price].present?)
+      raise ArgumentError, "Cannot search for a name and price at the same time"
+    elsif params[:min_price].present? && !params[:min_price].to_f.positive?
+      raise ArgumentError, "Cannot have a negative number for min_price"
+    elsif params[:max_price].present? && params[:max_price].to_f < 0
+      raise ArgumentError, "Max price cannot be 0 or lower than 0"
+    end
+  end
 end
