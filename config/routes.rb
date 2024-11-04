bcf7a87ddfb6561d67fab0abe60a6321d2f10d6f
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-11-04 15:48:26 -0600

    adjusted routes to new methods

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "api/v1/merchants", to: "api/v1/merchants#index"
-  get "api/v1/merchants/find", to: "api/v1/merchants#index"
+  get "api/v1/merchants/find", to: "api/v1/merchants#find"
   get "/api/v1/merchants/:id", to: "api/v1/merchants#show"
   post "/api/v1/merchants", to: "api/v1/merchants#create"
   delete "/api/v1/merchants/:id", to: "api/v1/merchants#destroy"
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get "/api/v1/merchants/:id/items", to: "api/v1/merchants_items#index"
 
   get "/api/v1/items", to: "api/v1/items#index"
-  get "/api/v1/items/find_all", to:  "api/v1/items#index"
+  get "/api/v1/items/find_all", to:  "api/v1/items#find_all"
   get "/api/v1/items/:id", to: "api/v1/items#show"
   patch "/api/v1/items/:id", to: "api/v1/items#update"
   put "/api/v1/items/:id", to: "api/v1/items#update"
