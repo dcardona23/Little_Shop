05bab6f61b6611126a5b5cb12efbb4f40dd55f82
Danielle Cardona <danielle.cardona23@gmail.com>
2024-11-07 16:17:21 -0700

    add coupons to Merchant class

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,7 @@
 class Merchant < ApplicationRecord
   validates :name, :presence => true
-  has_many :invoices
+  has_many :invoices, dependent: :destroy
+  has_many :coupons, dependent: :destroy
   has_many :items, dependent: :destroy
 
   def self.sort_by_age
