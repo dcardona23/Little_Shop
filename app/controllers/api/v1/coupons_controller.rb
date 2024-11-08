ff5a15bfd5019e66b5faeb8b9763182d37663dc6
Danielle Cardona <danielle.cardona23@gmail.com>
2024-11-08 09:08:35 -0700

    feat: add coupon index method and coupon serializer

--- app/controllers/api/v1/coupons_controller.rb
+++ app/controllers/api/v1/coupons_controller.rb
@@ -1,10 +1,9 @@
 class Api::V1::CouponsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
-  rescue_from ArgumentError, with: :invalid_parameters
 
   def index
     coupons = Coupon.all
-    render json: CouponSerializer.new
+    render json: CouponSerializer.new(coupons)
   end
 end
