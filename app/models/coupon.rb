66adecabbb618c22c56024772a347a97d3fc8da2
Danielle Cardona <danielle.cardona23@gmail.com>
2024-11-08 20:43:32 -0700

    feat: add create coupon method and test

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,6 +2,8 @@ class Coupon < ApplicationRecord
   belongs_to :merchant
   has_one :invoice
 
+  validates :name, :presence => true
+
   def self.filter_coupons(scope, params)
     scope = scope.where(merchant_id: params[:merchant_id]) if params[:merchant_id]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_08_003245) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_09_033959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_08_003245) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id", null: false
+    t.boolean "active"
     t.index ["merchant_id"], name: "index_coupons_on_merchant_id"
   end
 
