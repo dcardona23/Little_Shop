e8d178239128e7bb6eeadf604cc26b9b54e45ca3
Danielle Cardona <danielle.cardona23@gmail.com>
2024-11-11 17:19:16 -0700

    fix issues after db dump

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_11_234640) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_12_000909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,28 +30,27 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_11_234640) do
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "invoice_items", force: :cascade do |t|
-    t.bigint "invoice_id", null: false
-    t.string "item"
+    t.bigint "item_id"
+    t.bigint "invoice_id"
     t.integer "quantity"
     t.float "unit_price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
     t.index ["item_id"], name: "index_invoice_items_on_item_id"
   end
 
   create_table "invoices", force: :cascade do |t|
-    t.string "status"
     t.bigint "customer_id"
     t.bigint "merchant_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "status"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.bigint "coupon_id"
     t.index ["coupon_id"], name: "index_invoices_on_coupon_id"
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
@@ -62,9 +61,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_11_234640) do
     t.string "name"
     t.string "description"
     t.float "unit_price"
-    t.bigint "merchant_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.bigint "merchant_id"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
@@ -79,8 +78,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_11_234640) do
     t.string "credit_card_number"
     t.string "credit_card_expiration_date"
     t.string "result"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
@@ -89,7 +88,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_11_234640) do
   add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "coupons"
   add_foreign_key "invoices", "customers"
-  add_foreign_key "invoices", "merchants"
-  add_foreign_key "items", "merchants"
   add_foreign_key "transactions", "invoices"
 end
